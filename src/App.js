d5879b0ae7d21bce2849cdfd5c4d6157225f4f80
re-doc <xqcokid@gmail.com>
2023-03-12 12:19:12 +0800

    add dynamic sky

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react'
 import './App.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import Stats from 'three/addons/libs/stats.module.js';
 
 let container, stats;
-let camera, scene, renderer, uniforms, geometry, particleSystem, uniformsSky, resolution;
+let camera, scene, renderer, uniforms, geometry, particleSystem, uniformsSky, resolution, requestAnimationFrameTimer;
 const particles = 10000;
 const vertexshaderSky = `
   varying vec2 vUv;
   varying vec3 vNormal;
-  // varying vec3 fposition;
+  varying vec3 fposition;
 
   void main()	{
 
     vUv = uv;
     vNormal = normal;
     // gl_Position = vec4( position, 1.0 );
-
+    fposition = position;
     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
   }
@@ -24,29 +26,64 @@ const vertexshaderSky = `
 const fragmentshaderSky = `
   varying vec2 vUv;
   varying vec3 vNormal;
-  // varying vec3 fposition;
+  varying vec3 fposition;
 
   uniform float time;
   uniform vec3 color;
   uniform sampler2D colorTexture;
+  uniform vec3 lightDir;
 
   vec3 redColor = vec3(1,.5,0);
   vec3 blueColor = vec3(0,0,1);
 
   const mat2 m2 = mat2( 0.60, -0.80, 0.80, 0.60 );
 
+  vec3 Cloud(vec3 bgCol,vec3 ro,vec3 rd,vec3 cloudCol,float spd)
+    {
+        vec3 col = bgCol;
+        float t = time * 1.9 * spd;
+        vec2 sc = ro.xz + rd.xz*((1.)*50.0-ro.y)/rd.y;
+        vec2 p = 0.1*sc;
+        float f = 0.0;
+        float s = 0.5;
+        float sum =0.;
+        for(int i=0;i<2;i++){
+          p += t;
+          t *=1.5;
+          f += s*textureLod( colorTexture, p/256.0, 0.0).x; p = m2*p*2.02;
+          sum+= s;
+            s*=0.6;
+        }
+        float val = f/sum; 
+        col = mix( col, cloudCol, 0.5*smoothstep(0.5,0.8,val) );
+        return col;
+    }
+
   void main()	{
 
-    vec3 light = vec3( 0.5, 0.2, 1.0 );
-    light = normalize( light );
+    //vec3 light = vec3( 0.5, 0.2, 1.0 );
+    //light = normalize( light );
+    vec3 _lightDir = normalize(lightDir);
+    vec3 _fposition = normalize(fposition);
+    vec3 col = vec3(1.0,1.0,1.0);  
+    vec3 ro = vec3 (0.,0.,0.);
+    vec3 Dir = normalize( vec3(0.5,0.3,0.0) );
 
-    float dProd = dot( vNormal, light ) * 0.5 + 0.5;
-    float speed      = 1.0;
-    vec4 tcolor = texture2D( colorTexture, vUv );
-    vec4 gray = vec4( vec3( tcolor.r * 0.3 + tcolor.g * 0.59 + tcolor.b * 0.11 ) + (time*(speed/30.)), 1.0 );
+    //float dProd = dot( vNormal, light ) * 0.5 + 0.5;
+    //float speed      = 1.0;
+    //vec4 tcolor = texture2D( colorTexture, vUv );
+    //vec4 gray = vec4( vec3( tcolor.r * 0.3 + tcolor.g * 0.59 + tcolor.b * 0.11 ) + (time*(speed/30.)), 1.0 );
+    col = vec3(0.2,0.5,0.85)*1.0 - _fposition.y*_fposition.y;
+    col = Cloud(col,ro,_fposition,vec3(1.0,1.0,1.0),1.);
 
-    gl_FragColor = gray * vec4( vec3( dProd ) * vec3( color ), 1.0 );
+    float sundot = clamp(dot(_fposition,Dir),0.0,1.0);
+    col += 0.25*vec3(1.0,0.7,0.4)*pow( sundot,5.0 );
+    col += 0.25*vec3(1.0,0.8,0.6)*pow( sundot,64.0 );
+    col += 0.4*vec3(1.0,0.8,0.6)*pow( sundot,512.0 );
 
+    col = mix( col, 0.68*vec3(0.4,0.65,1.0), pow( 1.0-max(_fposition.y,0.0), 12.0 ) );
+    //gl_FragColor = gray * vec4( vec3( dProd ) * vec3( color ), 1.0 );
+    gl_FragColor = vec4(col,1.0);
   }
 `
 
@@ -80,22 +117,44 @@ const fragmentshader = `
 
   }
 `
-init().then( animate );
+
 
 function App() {
+  const renderRef  = useRef()
+
+  
+
+  useEffect(() => {
+    let _renderRefC
+    if (renderRef && renderRef.current) {
+      
+      init(renderRef).then( animate )
+
+      _renderRefC = renderRef.current
+   
+    }
+    return () => {
+      requestAnimationFrameTimer && cancelAnimationFrame(requestAnimationFrameTimer);
+      renderer.dispose()
+      _renderRefC.removeChild(_renderRefC.firstElementChild)
+    };
+ 
+  }, [renderRef]);
+
+
   return (
     <div className="App">
-      
+      <div ref={renderRef}></div>
     </div>
   );
 }
 
-async function init() {
+async function init(renderRef) {
 
   const { innerWidth, innerHeight } = window;
-
+  console.log(renderRef)
   container = document.createElement( 'div' );
-  document.body.appendChild( container );
+  renderRef.current.appendChild( container );
 
   // CAMERA
 
@@ -111,14 +170,25 @@ async function init() {
   // shader
   uniforms = {
 
-    pointTexture: { value: new THREE.TextureLoader().load( 'textures/sprites/spark1.png' ) }
+    pointTexture: { value: new THREE.TextureLoader().load( 'textures/spark1.png' ) }
 
   };
+
+  // LIGHTS
+
+  const light = new THREE.DirectionalLight( 0xaabbff, 0.3 );
+  light.position.x = 300;
+  light.position.y = 250;
+  light.position.z = - 500;
+  console.log('light', light);
+  scene.add( light );
+
   uniformsSky = {
     time: { value: 1.0 },
     'colorTexture': { value: new THREE.TextureLoader().load( 'textures/water.jpg' ) },
     'color': { value: new THREE.Color( 0xff2200 ) },
     iResolution: { value: resolution },
+    lightDir: light.position
   };
   // const shaderMaterial = new THREE.ShaderMaterial( {
   uniformsSky[ 'colorTexture' ].value.wrapS = uniformsSky[ 'colorTexture' ].value.wrapT = THREE.RepeatWrapping;
@@ -126,13 +196,7 @@ async function init() {
 
   // } );
 
-  // LIGHTS
 
-  const light = new THREE.DirectionalLight( 0x00BFFF, 0.3 );
-  light.position.x = 300;
-  light.position.y = 250;
-  light.position.z = - 500;
-  scene.add( light );
 
   // SKYDOME
 
@@ -203,7 +267,7 @@ async function init() {
 
 
 
-  scene.add( particleSystem );
+  // scene.add( particleSystem );
   // RENDERER
 
   renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -220,8 +284,8 @@ async function init() {
 
   // STATS
 
-  //stats = new Stats();
-  //container.appendChild( stats.dom );
+  stats = new Stats();
+  container.appendChild( stats.dom );
 
   // MODEL
 
@@ -248,13 +312,13 @@ function onWindowResize() {
 
 function animate() {
 
-  requestAnimationFrame( animate );
+  requestAnimationFrameTimer = requestAnimationFrame( animate );
 
   //nodeFrame.update();
 
   render();
 
-  //stats.update();
+  stats.update();
 
 }
 
